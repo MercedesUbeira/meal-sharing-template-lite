66a8360912d015d03560fbb2098103a0888a029a
Mercedes Ubeira Romero <mercedesubeira@gmail.com>
2023-05-19 19:48:31 +0200

    about design that looks great and is responsive

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -4,42 +4,44 @@ function About() {
   return (
     <div className="about-wrapper">
       <div className="about-container">
-        <div className="left-column">
-          <h2>About the App:</h2>
-          <p>
-            Our meal sharing app is more than just a platform for hosting and
-            sharing meals. Our primary objective is to{" "}
-            <strong>bring people together </strong>
-            and make the world a better place, one meal at a time. We believe
-            that sharing food is not only a way to satisfy hunger but also an
-            opportunity to <strong>connect with others</strong>, share cultures,
-            and create lasting memories.{" "}
-          </p>
-          <p>
-            {" "}
-            By facilitating connections between hosts and guests, our app
-            promotes cultural exchange and fosters a sense of community. Through
-            the shared experience of a home-cooked meal, people can learn about
-            different cuisines, traditions, and stories from around the world.
-            We believe that these interactions contribute to a more inclusive
-            and understanding society.{" "}
-          </p>
-          <p>
-            Whether you're a traveler seeking an authentic local dining
-            experience at an affordable price, a newcomer in town looking to
-            meet new people, or simply someone who enjoys trying out different
-            flavours, our Meal Sharing App is exactly what you need.
-          </p>
-          <p>
-            Start sharing today, and{" "}
-            <strong>make the world a better place.</strong>
-          </p>
-        </div>
-        <div className="right-column">
-          <img
-            src="https://media1.popsugar-assets.com/files/thumbor/RvGnZENog5JYnS_rwpNDQgVS8VM/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2021/09/30/698/n/47973216/tmp_HcFZ3H_14e8d87ddc1ad0c7_GettyImages-1173795388.jpg"
-            alt="girls sharing food"
-          />
+        <div className="row">
+          <div className="left-column">
+            <h2>About the App:</h2>
+            <p>
+              Our meal sharing app is more than just a platform for hosting and
+              sharing meals. Our primary objective is to{" "}
+              <strong>bring people together </strong>
+              and make the world a better place, one meal at a time. We believe
+              that sharing food is not only a way to satisfy hunger but also an
+              opportunity to <strong>connect with others</strong>, share
+              cultures, and create lasting memories.{" "}
+            </p>
+            <p>
+              {" "}
+              By facilitating connections between hosts and guests, our app
+              promotes cultural exchange and fosters a sense of community.
+              Through the shared experience of a home-cooked meal, people can
+              learn about different cuisines, traditions, and stories from
+              around the world. We believe that these interactions contribute to
+              a more inclusive and understanding society.{" "}
+            </p>
+            <p>
+              Whether you're a traveler seeking an authentic local dining
+              experience at an affordable price, a newcomer in town looking to
+              meet new people, or simply someone who enjoys trying out different
+              flavours, our Meal Sharing App is exactly what you need.
+            </p>
+            <p>
+              Start sharing today, and{" "}
+              <strong>make the world a better place.</strong>
+            </p>
+          </div>
+          <div className="right-column">
+            <img
+              src="https://media1.popsugar-assets.com/files/thumbor/RvGnZENog5JYnS_rwpNDQgVS8VM/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2021/09/30/698/n/47973216/tmp_HcFZ3H_14e8d87ddc1ad0c7_GettyImages-1173795388.jpg"
+              alt="girls sharing food"
+            />
+          </div>
         </div>
         <div className="row">
           <div className="left-column">
